ec50aa70dd041ab7689f0e3e8eef9345f5ca7c53
Johannes Kauhanen <z1master@hotmail.com>
2016-05-24 13:00:18 +0300

    Coveralls

--- app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
+++ app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
@@ -3,6 +3,8 @@ package fi.ohtu.mobilityprofile;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.util.HashSet;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
