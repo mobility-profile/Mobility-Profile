7d3c4dbe9c549a3afb37e3018d2625de2b098b20
iilumme <iilumme@cs.helsinki.fi>
2016-05-28 16:09:05 +0300

    some UI and permission requests

--- app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
+++ app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
@@ -1,15 +1,28 @@
 package fi.ohtu.mobilityprofile;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentPagerAdapter;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-import java.util.HashSet;
 
 public class MainActivity extends AppCompatActivity {
+
+    FragmentPagerAdapter adapterViewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+        adapterViewPager = new MyPagerAdapter(getSupportFragmentManager());
+        viewPager.setAdapter(adapterViewPager);
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
     }
 }
+
+
---
+++ app/src/main/java/fi/ohtu/mobilityprofile/MyPagerAdapter.java
@@ -0,0 +1,49 @@
+package fi.ohtu.mobilityprofile;
+
+import android.content.Context;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class MyPagerAdapter extends FragmentPagerAdapter {
+    private static final int NUM_ITEMS = 3;
+
+    public MyPagerAdapter(FragmentManager fragmentManager) {
+        super(fragmentManager);
+    }
+
+    @Override
+    public int getCount() {
+        return NUM_ITEMS;
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        switch (position) {
+            case 0:
+                return PrivacyFragment.newInstance();
+            case 1:
+                return ProfileFragment.newInstance();
+            case 2:
+                return InfoFragment.newInstance();
+            default:
+                return new Fragment();
+        }
+    }
+
+    // Returns the page title for the top indicator
+    @Override
+    public CharSequence getPageTitle(int position) {
+        switch (position) {
+            case 0:
+                return "PRIVACY";
+            case 1:
+                return "PROFILE";
+            case 2:
+                return "INFO";
+            default:
+                return "";
+        }
+    }
+
+}
