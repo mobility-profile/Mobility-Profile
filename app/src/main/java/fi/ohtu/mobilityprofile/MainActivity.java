574336cece96b2b56e7fefd609074cb0bb6800b9
etfairies <lilja.kettunen@live.fi>
2016-05-29 09:24:08 +0300

    connection to google calendar

--- app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
+++ app/src/main/java/fi/ohtu/mobilityprofile/MainActivity.java
@@ -1,7 +1,9 @@
 package fi.ohtu.mobilityprofile;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
 
 import java.util.HashSet;
 
@@ -11,5 +13,8 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent calendar = new Intent(MainActivity.this, CalendarActivity.class);
+        this.startActivity(calendar);
     }
 }
