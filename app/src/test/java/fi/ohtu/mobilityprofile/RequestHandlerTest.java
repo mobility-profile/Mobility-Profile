bf6651f3a54decfa548f3d2716a9d27a1a2b9f33
iilumme <iilumme@cs.helsinki.fi>
2016-06-16 13:41:25 +0300

    Test for searched route saving

---
+++ app/src/test/java/fi/ohtu/mobilityprofile/RequestHandlerTest.java
@@ -0,0 +1,62 @@
+package fi.ohtu.mobilityprofile;
+
+
+import android.content.Context;
+import android.os.Bundle;
+import android.os.Message;
+import android.os.Messenger;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.robolectric.Robolectric;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.annotation.Config;
+
+import static org.junit.Assert.*;
+
+import fi.ohtu.mobilityprofile.data.CalendarTagDao;
+import fi.ohtu.mobilityprofile.data.VisitDao;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+
+@RunWith(RobolectricTestRunner.class)
+@Config(manifest = "src/main/AndroidManifest.xml", constants = BuildConfig.class, sdk = 21)
+public class RequestHandlerTest {
+
+    private RequestHandler requestHandler;
+    private Context context;
+    private MobilityProfile mobilityProfile;
+    private CalendarTagDao calendarTagDao;
+    private VisitDao visitDao;
+    private RequestCode requestCode;
+
+    @Before
+    public void setUp() {
+        this.context = null;
+        this.mobilityProfile = mock(MobilityProfile.class);
+        this.calendarTagDao = mock(CalendarTagDao.class);
+        this.visitDao = new VisitDao();
+
+        this.requestHandler = new RequestHandler(context, mobilityProfile, calendarTagDao, visitDao);
+
+        when(mobilityProfile.isCalendarDestination()).thenReturn(false);
+
+        Robolectric.setupActivity(MainActivityStub.class);
+    }
+
+    @Test
+    public void insertsASearchedRoutetoDatabase() {
+        Bundle bundle = new Bundle();
+        bundle.putString(102+"", "Naantali");
+
+        Message msg = new Message();
+        msg.what = 102;
+        msg.setData(bundle);
+
+        requestHandler.handleMessage(msg);
+        assertTrue(visitDao.getLatestVisit().getLocation().equals("Naantali"));
+    }
+}
