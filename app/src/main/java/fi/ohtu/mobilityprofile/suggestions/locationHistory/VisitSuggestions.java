e9794c9685a188d9ea10f4d2602028cd2f22a73f
etfairies <triforce@windowslive.com>
2016-07-31 17:53:32 +0300

    added unit tests and squashed bugs

--- app/src/main/java/fi/ohtu/mobilityprofile/suggestions/locationHistory/VisitSuggestions.java
+++ app/src/main/java/fi/ohtu/mobilityprofile/suggestions/locationHistory/VisitSuggestions.java
@@ -8,7 +8,6 @@ import java.util.Map;
 
 
 import fi.ohtu.mobilityprofile.data.SignificantPlaceDao;
-import fi.ohtu.mobilityprofile.domain.SignificantPlace;
 import fi.ohtu.mobilityprofile.domain.Visit;
 
 import fi.ohtu.mobilityprofile.data.PlaceDao;
@@ -25,10 +24,12 @@ public class VisitSuggestions implements SuggestionSource {
     private List<Suggestion> suggestions;
     private Map<String, Integer> nextDestinations;
     private PlaceDao placeDao;
+    List<Visit> visits;
 
     public VisitSuggestions(SignificantPlaceDao dao, VisitDao visitdao) {
         this.significantPlaceDao = dao;
         this.visitdao = visitdao;
+        suggestions = new ArrayList<>();
     }
 
     /**
@@ -56,6 +57,12 @@ public class VisitSuggestions implements SuggestionSource {
 //                // This returns location with coordinates : "Liisankatu 1, Helsinki, Finland!00.0000!00.0000!"
 //            }
 //        }
+        visits = visitdao.getAllVisitsToSignificantPlaces();
+        // number of visits must be more than four so it can provide next, current, previous and before previous locations.
+        if (visits.size() > 4) {
+            calculateNextDestinations(startLocation);
+            calculateSuggestions();
+        }
 
         return suggestions;
     }
@@ -64,29 +71,22 @@ public class VisitSuggestions implements SuggestionSource {
      * Calculates next destinations based on visited SignificantPlaces in the past.
      * @param startLocation starting location
      */
-    private void calculateNextDestinations(SignificantPlace startLocation) {
-        List<Visit> visits = visitdao.getAllVisitsToSignificantPlaces();
-
-        // number of visits must be more than four so it can provide next, current, previous and before previous locations.
-        if (visits.size() <= 4) {
-            return;
-        }
+    private void calculateNextDestinations(String startLocation) {
 
         nextDestinations = new HashMap<>();
         String previousLocation = visits.get(1).getLocation();
-        String beforePrevious = visits.get(2).getLocation();
+        String beforePreviousLocation = visits.get(2).getLocation();
 
         // first and two last items are ignored because they do not have either next or previous and before previous location
         for (int i = 1; i < visits.size() - 2; i++) {
 
             // checks if startLocation is the same as the location currently examined in the list
             if (visits.get(i).getLocation().equals(startLocation)) {
-
                 // checks if the previous location in the past is the same as previous location from the current location
                 // and before previous location in the past is the same as before previous location from the current location
                 if ((visits.get(i + 1).getLocation().equals(previousLocation))
-                        && (visits.get(i + 2).getLocation().equals(beforePrevious))) {
-                    addDestinationToHashMap(visits.get(i - 1).getLocation());
+                        && (visits.get(i + 2).getLocation().equals(beforePreviousLocation))) {
+                    addToNextDestinations(visits.get(i - 1).getLocation());
                 }
              }
         }
@@ -97,7 +97,7 @@ public class VisitSuggestions implements SuggestionSource {
      * current location by one.
      * @param nextDestination possible next destination
      */
-    private void addDestinationToHashMap(String nextDestination) {
+    private void addToNextDestinations(String nextDestination) {
         int count = nextDestinations.containsKey(nextDestination) ? nextDestinations.get(nextDestination) : 0;
         nextDestinations.put(nextDestination, count + 1);
     }
@@ -108,10 +108,12 @@ public class VisitSuggestions implements SuggestionSource {
     private void calculateSuggestions() {
         suggestions = new ArrayList<>();
 
-        int maxValue = Collections.max(nextDestinations.values());
-        for (Map.Entry<String, Integer> entry : nextDestinations.entrySet()) {
-            if (entry.getValue() == maxValue) {
-                suggestions.add(new Suggestion(entry.getKey(), SuggestionAccuracy.HIGH, VISIT_SUGGESTIONS));
+        if (!nextDestinations.isEmpty()) {
+            int maxValue = Collections.max(nextDestinations.values());
+            for (Map.Entry<String, Integer> entry : nextDestinations.entrySet()) {
+                if (entry.getValue() == maxValue) {
+                    suggestions.add(new Suggestion(entry.getKey(), SuggestionAccuracy.HIGH, VISIT_SUGGESTIONS));
+                }
             }
         }
     }
